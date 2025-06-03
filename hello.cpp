011fe1459d6c5ec66bec8aaa70a6adbaa8cb77f2
LOGAN3000GTX <akmukhitov@gmail.com>
2025-06-03 19:51:32 +0600

    Update code again

--- hello.cpp
+++ hello.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main(){
-    cout << "Hello world Git" << endl;
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        sum += arr[i];
+    }
+    cout << "Sum of elements: " << sum << endl;
 }
