7a18e821f4b6b39816235d2dfeaad1bc309a1107
LOGAN3000GTX <akmukhitov@gmail.com>
2025-06-05 00:36:24 +0600

    Add Practice folder and practice.py file

---
+++ practice/practice.py
@@ -0,0 +1,27 @@
+"""
+import random
+print(random.randrange(1, 100))
+"""
+
+"""
+ages = [20, 21, 22]
+a, u, i = ages
+print(u+a)
+"""
+
+"""
+x = 2025 #int
+y = 21.3 #float
+z = "Aaron" #str
+q = 2j+3j+55 # complex
+print(type(x)) # function type() detect which is variable? int or str or comple etc.
+print(type(y))
+print(type(z))
+print(type(q))
+"""
+
+txt = str(input())
+if "Aaron" in txt:
+     print("yes")
+else:
+     print("no")
