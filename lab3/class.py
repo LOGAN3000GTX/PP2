2810339e89e39228764e9ffbcc8452b9fc1fb528
Aaron Mukhitov <87278586+LOGAN3000GTX@users.noreply.github.com>
2025-06-11 13:17:48 +0600

    Add files via upload

--- lab3/class.py
+++ lab3/class.py
@@ -3,3 +3,17 @@ class Point:
 
 p = Point()
 print(type(p.x))
+
+class MyString:
+    def __init__(self):
+        self.s = ""
+
+    def getString(self):
+        self.s = input("Введите строку: ")
+
+    def printString(self):
+        print(self.s.upper())
+
+obj = MyString()
+obj.getString()
+obj.printString()
